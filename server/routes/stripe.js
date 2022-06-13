d41c6f7f16bb338f04d80d0a3f494afca52df726
Kartik <58771275+kartikgarg08@users.noreply.github.com>
2022-06-13 22:03:06 +0530

    Add files via upload

--- server/routes/stripe.js
+++ server/routes/stripe.js
@@ -1,12 +1,16 @@
 const router = require("express").Router();
-const stripe = require("stripe")(process.env.STRIPE_KEY);
+// const stripe = require("stripe")(process.env.STRIPE_KEY);
+const KEY = process.env.STRIPE_KEY
+const stripe = require("stripe")(KEY);
 
 router.post("/payment", (req, res) => {
-    stripe.charges.create({
+    stripe.charges.create(
+        {
             source: req.body.tokenId,
             amount: req.body.amount,
             currency: "usd",
-        }, (stripeErr, stripeRes) => {
+        }, 
+        (stripeErr, stripeRes) => {
             if (stripeErr) {
                 res.status(500).json(stripeErr);
             } else {
